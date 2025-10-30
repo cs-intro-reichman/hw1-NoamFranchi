a727296013aa1f26a4646b553a502c4819d1d53a
Noam Franchi <noam@Noams-MacBook-Air.local>
2025-10-30 11:40:03 +0200

    2 improved

--- FVCalc.java
+++ FVCalc.java
@@ -4,8 +4,8 @@ public class FVCalc {
 		int currentValue = Integer.parseInt(args[0]); //invested sum
 		double rate = Double.parseDouble(args[1]);
 		int n = Integer.parseInt(args[2]); // years
-		double futureValue = currentValue * Math.pow((1 + rate),n);
+		double futureValue = currentValue * Math.pow((1 + rate / 100),n);
 		System.out.println("After " + n + " years, $" + currentValue + " saved at " + rate + "% will yield $" + (int)futureValue);
-		 
+
 	}
 }
